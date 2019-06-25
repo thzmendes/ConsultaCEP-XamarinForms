9b3cf76c47de8c1e2eb8fa1a7921f3f56be08815
tmendes <th.mendes1997@gmail.com>
2019-06-25 17:24:37 -0300

    Init

---
+++ ConsultarCEP/ConsultarCEP/ConsultarCEP/Service/Model/Address.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsultarCEP.Service.Model
+{
+    public class Address
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string unidade { get; set; }
+        public string ibge { get; set; }
+        public string gia { get; set; }
+
+        public Address(string cEP, string lagradouro, string complemento, string bairro, string cidade, string uF, string unidade, string iBGE, string gIA)
+        {
+            cep = cEP;
+            logradouro = lagradouro;
+            this.complemento = complemento;
+            this.bairro = bairro;
+            localidade = cidade;
+            uf = uF;
+            this.unidade = unidade;
+            ibge = iBGE;
+            gia = gIA;
+        }
+    }
+}
---
+++ ConsultarCEP/ConsultarCEP/ConsultarCEP/Service/ViaCepService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ConsultarCEP.Service.Model;
+using Newtonsoft.Json;
+
+
+namespace ConsultarCEP.Service
+{
+    public class ViaCepService
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Address BuscarEnderecoViaCEP(string cep)
+        {
+            string NovaURL = string.Format(EnderecoURL,cep);
+            WebClient wc = new WebClient();
+            string content = wc.DownloadString(NovaURL);
+            Address address = JsonConvert.DeserializeObject<Address>(content);
+            return address;
+        }
+    }
+}
